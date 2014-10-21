389c7d6bfb533c5947ac485cc42fa3347e2dc857
Lukasz Czaplicki <lukasz@theappbusiness.com>
2014-10-21 17:11:13 +0100

    robolectic setup

---
+++ app/src/androidTest/java/com/fada21/android/bootstrap/HomeActivityTest.java
@@ -0,0 +1,22 @@
+package com.fada21.android.bootstrap;
+
+import android.app.Activity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+@Config(manifest = "./app/src/main/AndroidManifest.xml")
+@RunWith(RobolectricTestRunner.class)
+public class HomeActivityTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        Activity activity = Robolectric.buildActivity(HomeActivity.class).create().get();
+        assertTrue(activity == null);
+    }
+}
